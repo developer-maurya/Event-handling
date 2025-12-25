951a4f0bb3a0dfb4bf3b3f5fe0a60c841e238f60
Chitranjan kumar <chitranjan75075@gmail.com>
2025-12-25 23:49:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,41 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+  // function handClick() {
+  //   alert("I am click")
+    
+  // }
+
+  // function hover (){
+  //   alert("tuch")
+  // }
+
+  function changeInput(params) {
+    console.log("change input")
+    
+  }
+
+  return (
+    <>
+    <div>
+
+    <form>
+      <input type='text' onChange={changeInput}/>
+    </form>
+
+    {/* <p onMouseOver={hover} style={{border:"1px solid black"}}> 
+      Hi Chitranjan
+    </p>
+      
+       <button onClick={() => {alert("i am click")}}>click me</button> */}
+      </div>
+    </>
+  )
+}
+
+export default App
 
